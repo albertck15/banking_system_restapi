07ca08fdf229e9e141c9603817282ffa7ced1126
Albert Csercsák <albertck15@gmail.com>
2024-02-15 19:14:16 +0100

    Uploading my REST Project

---
+++ src/main/java/hu/csercsak_albert/banking_system/entity/User.java
@@ -0,0 +1,51 @@
+package hu.csercsak_albert.banking_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String hashedPassword;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAtDate;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAtDate;
+
+    @Column(nullable = false)
+    private LocalDateTime lastLoginDate;
+
+}
---
+++ src/test/java/hu/csercsak_albert/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.csercsak_albert.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
